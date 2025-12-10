eafa69fdce1bff6a096c1f3497c74f0d59195f6d
amartello <lunafreelancerr@gmail.com>
2025-12-10 00:36:35 -0300

    Ejercicio 3 – Crear tabla mascotas

--- tp.sql
+++ tp.sql
@@ -10,3 +10,12 @@ CREATE TABLE duenos (
     telefono VARCHAR(20) NOT NULL,
     direccion VARCHAR(100)
 );
+-- Ejercicio 3: Crear tabla mascotas
+CREATE TABLE mascotas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    especie VARCHAR(30) NOT NULL,
+    fecha_nacimiento DATE,
+    id_dueno INT,
+    FOREIGN KEY (id_dueno) REFERENCES duenos(id)
+);
