91251d6cf00c1f65902abafb8dd2b660fbac6b86
amartello <lunafreelancerr@gmail.com>
2025-12-16 02:07:29 -0300

    ejercicio 7- update

--- tp.sql
+++ tp.sql
@@ -1,7 +1,6 @@
 -- Ejercicio 1: Crear base de datos
 CREATE DATABASE veterinaria_patitas_felices;
 USE veterinaria_patitas_felices;
-
 -- Ejercicio 2: Crear tabla duenos
 CREATE TABLE duenos (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -37,32 +36,40 @@ CREATE TABLE historial_clinico (
     FOREIGN KEY (id_mascota) REFERENCES mascotas(id),
     FOREIGN KEY (id_veterinario) REFERENCES veterinarios(id)
 );
-
 -- Ejercicio 6 - Insertar registros
-
 -- 3 dueños
 INSERT INTO duenos (nombre, apellido, telefono, direccion)
-VALUES 
-('Laura', 'Perez', '1123335467', 'Arenales 2987'),
-('Julio', 'Zambrano', '112345678', 'Florida 456'),
-('Lucía', 'Vera', '113456789', 'Laprida 789');
-
+VALUES ('Laura', 'Perez', '1123335467', 'Arenales 2987'),
+    ('Julio', 'Zambrano', '112345678', 'Florida 456'),
+    ('Lucía', 'Vera', '113456789', 'Laprida 789');
 -- 3 mascotas 
 INSERT INTO mascotas (nombre, especie, fecha_nacimiento, id_dueno)
-VALUES
-('Bubi', 'Gato', '2020-05-10', 1),   -- Dueña: Laura
-('Pancito Bebe', 'Gato', '2019-08-22', 2),       -- Dueño: Julio
-('Manchita', 'Perro', '2021-01-15', 3);      -- Dueña: Lucía
-
+VALUES ('Bubi', 'Gato', '2020-05-10', 1),
+    -- Dueña: Laura
+    ('Pancito Bebe', 'Gato', '2019-08-22', 2),
+    -- Dueño: Julio
+    ('Manchita', 'Perro', '2021-01-15', 3);
+-- Dueña: Lucía
 -- 2 veterinarios con especialidades distintas
 INSERT INTO veterinarios (nombre, apellido, matricula, especialidad)
-VALUES
-('Alan', 'Martínez', 'MAT123', 'Cirugía'),
-('Josefina', 'Aguero', 'MAT456', 'Clinica');
-
+VALUES ('Alan', 'Martínez', 'MAT123', 'Cirugía'),
+    ('Josefina', 'Aguero', 'MAT456', 'Clinica');
 -- 3 registros de historial clínico
 INSERT INTO historial_clinico (id_mascota, id_veterinario, descripcion)
-VALUES
-(1, 1, 'Vacunación anual'),
-(2, 2, 'Desparasitación'),
-(3, 1, 'Cirugía de esterilización');
+VALUES (1, 1, 'Vacunación anual'),
+    (2, 2, 'Desparasitación'),
+    (3, 1, 'Cirugía de esterilización');
+-- Ejercicio 7 - Update
+--1) Cambiar la dirección de un dueño (por ID)
+UPDATE duenos
+SET direccion = 'Av. Nazca 788'
+WHERE id = 1;
+--2) Actualizar la especialidad de un veterinario (por matrícula).
+UPDATE veterinarios
+SET especialidad = 'Tecnico Laboratorio'
+WHERE matricula = 'MAT456';
+--3) Editar la descripción de un historial clínico (por ID)
+UPDATE historial_clinico
+SET descripcion = 'Chequeo general'
+WHERE id = 2;
+-- Ejercicio 8 -
