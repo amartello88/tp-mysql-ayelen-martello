4641449993c0ce549d707194d2837f261a86851a
amartello <lunafreelancerr@gmail.com>
2025-12-16 11:47:53 -0300

    agregue el orden por reg desc porq o lo habia visto

--- tp.sql
+++ tp.sql
@@ -94,4 +94,5 @@ SELECT m.nombre AS NombreMascota,
 FROM historial_clinico h
     JOIN mascotas m ON h.id_mascota = m.id
     JOIN duenos d ON m.id_dueno = d.id
-    JOIN veterinarios v ON h.id_veterinario = v.id;
+    JOIN veterinarios v ON h.id_veterinario = v.id
+ORDER BY h.fecha_registro DESC;
